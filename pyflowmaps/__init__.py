2e6f8cdfe62e8abab5b47244b8f6be2b24163c1d
Hypnus1803 <hypnus1803@gmail.com>
2019-10-04 15:03:40 +0200

    editing __init__ flie

--- pyflowmaps/__init__.py
+++ pyflowmaps/__init__.py
@@ -2,5 +2,7 @@
 pyflowmaps is a package that infers the horizontal, and vertical velocities
 from intensity filtegrams, as well as from magnetograms obtained from the Sun
 """
-
-from .pyflowmaps import flow
+__version__ = "2019.10.04"
+__authors__ = "Jose Ivan Campos Rozo"
+from .flow import *
+from .math_tools import *
